0d831eddb871385b433e45dd25d4a3ac94adfaf8
Zayd <zaydkassimi01@gmail.com>
2024-09-08 14:41:33 +0100

    update file json call assets when i export food list and add some information

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -118,3 +118,262 @@ export const menu_list = [
         menu_image: menu8
     },
 ]
+
+export const food_list = [
+    {
+        _id:"1",
+        name:"Neapolitan Pizza",
+        image: food1,
+        price: 12,
+        description:"Neapolitan pizza, or pizza Napoletana, is a type of pizza that originated in Naples, Italy. This style of pizza is prepared with simple and fresh ingredients: a basic dough, raw tomatoes, fresh mozzarella cheese, fresh basil, and olive oil.",
+        category:"Pizza"
+    },
+    {
+        _id:"2",
+        name:"Sicilian Pizza",
+        image: food2,
+        price: 19,
+        description:"Traditional Sicilian pizza is often thick-crusted and rectangular, but can also be round and similar to the Neapolitan pizza. It is often topped with onions, anchovies, tomatoes, herbs and strong cheese such as caciocavallo and toma. Other versions do not include cheese.",
+        category:"Pizza"
+    },
+    {
+        _id:"3",
+        name:"Barrel Boy Pizza",
+        image: food3,
+        price: 27,
+        description:"Barrel Boy, this pizza is loaded with ricotta, Italian sausage, pepperoni, roasted red pepper, basil, mozzarella, and our signature sauce,",
+        category:"Pizza"
+    },
+    {
+        _id:"4",
+        name:"Vegetarian Pizza",
+        image: food4,
+        price: 10,
+        description:"It's fresh and full of flavor, featuring cherry tomatoes, artichoke, bell pepper, olives, red onion and some hidden (and optional) baby spinach. You'll find a base of rich tomato sauce and golden, bubbling mozzarella underneath, of course.",
+        category:"Pizza"
+    },
+    {
+        _id:"5",
+        name:"Butter Chicken",
+        image: food5,
+        price: 70,
+        description:"This chicken dinner uses deeply flavored chicken thighs, simmered in an ultra-silky and aromatic sauce of tomatoes, cream, and cashews. The cashews lend a buttery nuttiness and great texture to the sauce in this recipe, but blanched almonds would also work well. For the ultimate weeknight dinner, spoon this dish over steamed rice or serve with warm naan.",
+        category:"Chicken"
+    },
+    {
+        _id:"6",
+        name:"Chicken Florentine Pasta",
+        image: food6,
+        price: 40,
+        description:"If you're obsessed with chicken Florentine, then let us introduce you to your new favorite pasta dinner. Ready in under an hour, this is the weeknight dinner to make when you're craving something a bit special.",
+        category:"Chicken"
+    },
+    {
+        _id:"7",
+        name:"Orange Chicken",
+        image: food7,
+        price: 45,
+        description:"If you just can't get enough of the classic Panda Express dish, this copycat recipe will be a game-changer. Ready in under an hour with all of the same sweet and savory flavors you love, staying in for weeknight dinner has never been easier.",
+        category:"Chicken"
+    },
+    {
+        _id:"8",
+        name:"Chicken Enchiladas",
+        image: food8,
+        price: 100,
+        description:"When the craving for satisfying comfort food meets the need for a straightforward dinner, chicken enchiladas are a guaranteed winner. They’re easy, filling, and take less than an hour to prepare from start to finish.",
+        category:"Chicken"
+    },
+    {
+        _id:"9",
+        name:"Taco al Pastor",
+        image: food9,
+        price: 40,
+        description:"A popular Mexican taco featuring pork marinated in a blend of spices, pineapple, and achiote. The pork is cooked on a vertical spit (like shawarma) and shaved onto corn tortillas, creating a sweet, savory, and slightly spicy flavor profile.",
+        category:"Tacos"
+    },
+    {
+        _id:"10",
+        name:"Tacos de Asada",
+        image: food10,
+        price: 60,
+        description:"Made with grilled beef, often skirt or flank steak, chopped and served on corn tortillas with onions, cilantro, and salsa. The beef gives these tacos a smoky, savory flavor with a juicy, meaty texture.",
+        category:"Tacos"
+    },
+    {
+        _id:"11",
+        name:"Tacos de Carnitas",
+        image: food11,
+        price: 30,
+        description:"These tacos are made from pork that’s been slow-cooked in its own fat until tender, then shredded. The rich, savory pork is often crispy and is typically garnished with onions, cilantro, and salsa.",
+        category:"Tacos"
+    },
+    {
+        _id:"12",
+        name:"Tacos de Barbacoa",
+        image: food12,
+        price: 90,
+        description:"Traditionally made with slow-cooked lamb or goat, barbacoa tacos have a tender, juicy texture and often feature a smoky, deep flavor. Beef can also be used in certain regions, and they’re usually served with simple toppings like onions and cilantro.",
+        category:"Tacos"
+    },
+    {
+        _id:"13",
+        name:"Spaghetti",
+        image: food13,
+        price: 15,
+        description:"A long, thin, cylindrical pasta, perhaps the most well-known type. It's commonly paired with tomato-based sauces, meatballs, or olive oil and garlic, making it versatile and ideal for a variety of Italian dishes.",
+        category:"Pasta"
+    },
+    {
+        _id:"14",
+        name:"Penne",
+        image: food14,
+        price: 25,
+        description:"A tube-shaped pasta with diagonal cuts at both ends, penne is perfect for holding onto thick, creamy, or chunky sauces. It works well in baked dishes, such as pasta al forno, or with hearty vegetable or meat-based sauces.",
+        category:"Pasta"
+    },
+    {
+        _id:"15",
+        name:"Fettuccine",
+        image: food15,
+        price: 10,
+        description:"A flat, thick noodle similar to a ribbon, often served with rich, creamy sauces like Alfredo. Fettuccine is hearty and pairs well with ingredients like chicken or mushrooms, adding texture and flavor to heavier dishes.",
+        category:"Pasta"
+    },
+    {
+        _id:"16",
+        name:"Fusilli",
+        image: food16,
+        price: 5,
+        description:" A spiral-shaped pasta that’s excellent at holding onto sauces, fusilli works well with chunky vegetable or meat sauces. Its twisted shape also makes it perfect for pasta salads, allowing it to capture more dressing or toppings.",
+        category:"Pasta"
+    },
+    {
+        _id:"17",
+        name:"Traditional Couscous",
+        image: food17,
+        price: 50,
+        description:"Often referred to as Moroccan couscous, this is the most common type. It consists of small steamed granules of wheat, typically served with a variety of stews, vegetables, and meats. The texture is light and fluffy, making it a staple in North African cuisine.",
+        category:"couscous"
+    },
+    {
+        _id:"18",
+        name:"Lebanese Couscous",
+        image: food18,
+        price: 30,
+        description:"Known as moghrabieh or majadra, Lebanese couscous features larger, rounder grains than traditional couscous. It's typically cooked with meats and vegetables or used in hearty dishes. The texture is somewhat chewy, and it has a nutty flavor.",
+        category:"couscous"
+    },
+    {
+        _id:"19",
+        name:"Algerian Couscous",
+        image: food19,
+        price: 35,
+        description:"Similar to Moroccan couscous, Algerian couscous is made from steamed granules but may feature regional variations in seasoning and accompaniments. It often includes spicy and rich stews, sometimes with ingredients like chickpeas and a mix of local spices, reflecting Algerian culinary traditions.",
+        category:"couscous"
+    },
+    {
+        _id:"20",
+        name:"Tunisian Couscous",
+        image: food20,
+        price: 40,
+        description:"Known for its spiciness, Tunisian couscous is often prepared with a fiery chili paste called harissa, along with lamb, chicken, or fish. The couscous is typically served with a rich, spicy broth and a variety of vegetables, reflecting Tunisia’s bold flavors.",
+        category:"couscous"
+    },
+    {
+        _id:"21",
+        name:"Tajine of Lamb with Prunes",
+        image: food21,
+        price: 80,
+        description:"This tajine features tender lamb cooked with sweet prunes, almonds, and a blend of spices such as cinnamon and saffron. The result is a rich, sweet-savory dish with a fragrant and complex flavor profile.",
+        category:"tajine"
+    },
+    {
+        _id:"22",
+        name:"Tajine of Chicken with Lemon and Olives",
+        image: food22,
+        price: 60,
+        description:"A classic Moroccan dish, this tajine combines chicken with preserved lemons and green olives. The slow cooking process melds the tangy flavors of the lemon with the briny olives, creating a savory and aromatic dish.",
+        category:"tajine"
+    },
+    {
+        _id:"23",
+        name:"Tajine of Beef with Vegetables",
+        image: food23,
+        price: 90,
+        description:"This version includes chunks of beef stewed with a variety of vegetables such as carrots, potatoes, and zucchini. The dish is flavored with a mix of spices like cumin, coriander, and turmeric, resulting in a hearty and flavorful meal.",
+        category:"tajine"
+    },
+    {
+        _id:"24",
+        name:"Tajine of Fish with Tomatoes and Onions",
+        image: food24,
+        price: 30,
+        description:"This tajine is made with fish, often layered with tomatoes, onions, and a blend of spices such as paprika and garlic. The slow cooking process infuses the fish with the rich flavors of the vegetables and spices, creating a light and flavorful dish.",
+        category:"tajine"
+    },
+    {
+        _id:"25",
+        name:"Salmon",
+        image: food25,
+        price: 100,
+        description:"Known for its rich, oily flesh and high omega-3 fatty acid content, salmon is a popular choice for its flavor and nutritional benefits. It's often enjoyed grilled, baked, or smoked.",
+        category:"fish"
+    },
+    {
+        _id:"26",
+        name:"Cod",
+        image: food26,
+        price: 120,
+        description:"Cod has a mild, slightly sweet flavor and a flaky texture. It's commonly used in dishes like fish and chips and is also popular in soups and stews.",
+        category:"fish"
+    },
+    {
+        _id:"27",
+        name:"Tuna",
+        image: food27,
+        price: 105,
+        description:"Tuna is a versatile fish with a firm texture and mild flavor. It's commonly used in sushi, salads, and steaks. There are various types, such as yellowfin and bluefin, each with distinct characteristics.",
+        category:"fish"
+    },
+    {
+        _id:"28",
+        name:"Halibut",
+        image: food28,
+        price: 80,
+        description:"Halibut is a large flatfish with a mild, sweet flavor and firm, white flesh. It's often enjoyed grilled, baked, or pan-seared. Its firm texture makes it suitable for various cooking methods.",
+        category:"fish"
+    },
+    {
+        _id:"29",
+        name:"Pastilla de Pollo (Chicken Pastilla)",
+        image: food29,
+        price: 150,
+        description:"This is the most well-known version of pastilla. It features shredded chicken cooked with onions, garlic, and a blend of spices such as cinnamon, ginger, and saffron. The chicken mixture is combined with almonds and sometimes eggs, then wrapped in layers of phyllo pastry and baked until golden and crispy. It's often garnished with powdered sugar and cinnamon, giving it a sweet and savory flavor.",
+        category:"pastilla"
+    },
+    {
+        _id:"30",
+        name:"Pastilla de Pigeon (Pigeon Pastilla)",
+        image: food30,
+        price: 130,
+        description:"Traditionally, this version uses pigeon, which is slow-cooked with similar spices as chicken pastilla. The pigeon meat is shredded and mixed with almonds, eggs, and a blend of aromatic spices. Like its chicken counterpart, it is wrapped in phyllo pastry and baked until crisp. This version is considered more authentic and is often reserved for special occasions.",
+        category:"pastilla"
+    },
+    {
+        _id:"31",
+        name:"Pastilla de Mariscos (Seafood Pastilla)",
+        image: food31,
+        price: 110,
+        description:"This variation uses a mix of seafood, such as shrimp, fish, and sometimes squid. The seafood is cooked with spices, herbs, and often a touch of lemon. The mixture is then combined with ingredients like onions and sometimes nuts, wrapped in phyllo pastry, and baked. This pastilla is lighter and offers a different flavor profile compared to the traditional meat versions.",
+        category:"pastilla"
+    },
+    {
+        _id:"32",
+        name:"Pastilla Végétarienne (Vegetarian Pastilla)",
+        image: food32,
+        price: 170,
+        description:"A vegetarian version of pastilla typically includes a variety of vegetables like mushrooms, spinach, and sometimes legumes such as chickpeas. The vegetables are cooked with Moroccan spices and mixed with nuts and dried fruits. This mixture is then wrapped in phyllo pastry and baked, providing a flavorful and satisfying option for those who prefer not to eat meat.",
+        category:"pastilla"
+    }
+]
